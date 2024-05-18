ee19d641015b19ec3444c78331df953cb53ee12e
Prashant Chothani <prashant.chothani@travelxp.tv>
2024-05-18 18:17:44 +0530

    refactor: added X-Response-Time header

--- api/index.ts
+++ api/index.ts
@@ -7,6 +7,15 @@ export const config = {
 
 const app = new Hono().basePath('/api')
 
+// Add X-Response-Time header
+app.use('*', async (c, next) => {
+	const start = Date.now()
+	await next()
+	const ms = Date.now() - start
+	c.header('X-Response-Time', `${ms}ms`)
+})
+
+
 app.get('/', (c) => {
   return c.json({ message: 'Hello Hono!' })
 })
