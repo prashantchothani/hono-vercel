6b9d6b6eefaa7897fa242a742eeca80adda2fd07
Prashant Chothani <prashant.chothani@travelxp.tv>
2024-05-19 14:03:41 +0530

    refactor: commented profile controller

--- api/index.js
+++ api/index.js
@@ -11,13 +11,13 @@ const app = new Hono().basePath('/api')
 // Add X-Response-Time header
 app.use('*', async (c, next) => {
 	const start = Date.now()
-	const REQUEST_COUNTRY_CODE = c.req.header('x-vercel-ip-country')
+	const REQUEST_COUNTRY = c.req.header('x-vercel-ip-country')
 	const REQUEST_CITY = c.req.header('x-vercel-ip-city')
 	const REQUEST_COUNTRY_REGION = c.req.header('x-vercel-ip-country-region')
 	const REQUEST_LATITUDE = c.req.header('x-vercel-ip-latitude')
 	const REQUEST_LONGITUDE = c.req.header('x-vercel-ip-longitude')
 
-	c.set("GLOBAL_REQUEST_COUNTRY_CODE", REQUEST_COUNTRY_CODE)
+	c.set("GLOBAL_REQUEST_COUNTRY", REQUEST_COUNTRY)
 	c.set("GLOBAL_REQUEST_CITY", REQUEST_CITY)
 
 	await next()
@@ -39,10 +39,11 @@ app.onError((err, c) => {
 })
 
 
-app.route('/profile', profile)
+// app.route('/profile', profile)
 
 app.get('/', (c) => {
-	return c.json({ message: 'Hello Hono!' })
+	const GLOBAL_REQUEST_CITY = c.get('GLOBAL_REQUEST_CITY')
+	return c.json({ message: 'Hello Hono ! City : ' + GLOBAL_REQUEST_CITY })
 })
 
 export default handle(app)
